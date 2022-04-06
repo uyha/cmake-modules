0ba0db3eec4aa51c03aef7f30240b3b3519429df
Uy Ha <hchanuy@gmail.com>
2022-04-06 23:49:02 +0200

    Fix auto-gen-pull-modules.ps1 script

--- tools/auto-gen-pull-modules.ps1
+++ tools/auto-gen-pull-modules.ps1
@@ -5,8 +5,8 @@ $IsNew=$LastExitCode
 git diff --exit-code $PSScriptRoot/pull-modules.ps1 2>&1 | Out-Null
 $HasChanged=$LastExitCode
 
-Write-Output "$IsNew $HasChanged"
 if($IsNew -ne 0 -or $HasChanged -ne 0){
-  git commit "$PSScriptRoot/gen-pull-modules.sh" -m 'Regenerate pull-modules.ps1'
+  git add "$PSScriptRoot/pull-modules.ps1"
+  git commit -m 'Regenerate pull-modules.ps1'
   git push origin "$(git rev-parse --abbrev-ref HEAD)"
 }
