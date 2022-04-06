c62ef922b1c1768de95cde215a930132d0749f12
Uy Ha <hchanuy@gmail.com>
2022-04-06 23:28:08 +0200

    Adjust scripts a bit

--- tools/auto-gen-pull-modules.sh
+++ tools/auto-gen-pull-modules.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 "$SCRIPT_DIR/gen-pull-modules.sh"
 
 if ! git diff --exit-code tools/pull-modules.sh >/dev/null; then
-  git commit tools/pull-modules.sh -m 'Regenerate pull-modules.sh'
+  git commit "$SCRIPT_DIR/pull-modules.sh" -m 'Regenerate pull-modules.sh'
   git push origin "$(git rev-parse --abbrev-ref HEAD)"
 fi
--- tools/gen-pull-modules.sh
+++ tools/gen-pull-modules.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-output=tools/pull-modules.sh
+output="$SCRIPT_DIR"/pull-modules.sh
 
 shopt -s globstar
 modules=(**/*.cmake)
 modules=("${modules[@]/%/'"'}")
 modules=("${modules[@]/#/'"'}")
 
-cat >$output <<EOF
+cat >"$output" <<EOF
 #!/usr/bin/env bash
 
 set -euo pipefail
 
 repo="https://github.com/uyha/cmake-modules"
-raw_repo="https://raw.githubusercontent.com/uyha/cmake-modules"
+raw_repo="https://raw.githubusercontent.com/uyha/cmake-modules/master"
 modules=(
 $(printf '  %s\n' "${modules[@]}")
 )
@@ -27,5 +28,5 @@ printf "%s\n" "\${modules[@]}" |
          --location \\
          --create-dirs \\
          --output cmake/{} \\
-         \$raw_repo/master/{}
+         \$raw_repo/{}
 EOF
