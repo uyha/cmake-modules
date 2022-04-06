839cc9478221ef0ed0740431f6d46bc77236e5cc
Uy Ha <hchanuy@gmail.com>
2022-04-06 23:54:28 +0200

    Update auto-gen-pull-modules.sh script

--- tools/auto-gen-pull-modules.sh
+++ tools/auto-gen-pull-modules.sh
@@ -5,7 +5,12 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 "$SCRIPT_DIR/gen-pull-modules.sh"
 
-if ! git diff --exit-code tools/pull-modules.sh >/dev/null; then
+git ls-files --error-unmatch "$SCRIPT_DIR/pull-modules.sh" 1>/dev/null 2>&1
+is_new=$?
+git diff --exit-code tools/pull-modules.sh 1>/dev/null 2>&1
+has_changed=$?
+
+if [[ $is_new -ne 0 || $has_changed -ne 0 ]]; then
   git commit "$SCRIPT_DIR/pull-modules.sh" -m 'Regenerate pull-modules.sh'
   git push origin "$(git rev-parse --abbrev-ref HEAD)"
 fi
