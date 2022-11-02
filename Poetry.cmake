2664cd820fd3e0bf5f0bfd9c432e7e7a0414f3e9
Uy Ha <hchanuy@gmail.com>
2022-11-02 12:44:09 +0000

    skip poetry when no pyproject.toml is found

--- Poetry.cmake
+++ Poetry.cmake
@@ -1,4 +1,10 @@
 find_package(Poetry REQUIRED)
+
+if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml")
+  message(STATUS "No pyproject.toml found, skipping setting Poetry up")
+  return()
+endif()
+
 if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/poetry.lock")
     file(COPY "${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}")
     poetry_install(DIR "${CMAKE_CURRENT_BINARY_DIR}" OPTIONS "--no-root" "--only" "main")
@@ -6,8 +12,10 @@ elseif("${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml" IS_NEWER_THAN "${CMAKE_CURRE
     file(COPY "${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}")
     poetry_update(DIR "${CMAKE_CURRENT_BINARY_DIR}" OPTIONS "--no-dev")
 endif()
+
 poetry_path(Python_ROOT_DIR)
 set(Python_ROOT_DIR "${Python_ROOT_DIR}" CACHE PATH "Path to the root directory of the target Python interpreter")
+
 set_property(
   DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
   APPEND
