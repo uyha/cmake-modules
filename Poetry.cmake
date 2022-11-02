91ff336c27ea520ee5c593563e640227c7d13e4c
Uy Ha <hchanuy@gmail.com>
2022-11-02 11:56:38 +0000

    switch to using '--only main' instead of '--no-dev'

--- Poetry.cmake
+++ Poetry.cmake
@@ -1,7 +1,7 @@
 find_package(Poetry REQUIRED)
 if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/poetry.lock")
     file(COPY "${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}")
-    poetry_install(DIR "${CMAKE_CURRENT_BINARY_DIR}" OPTIONS "--no-root" "--no-dev")
+    poetry_install(DIR "${CMAKE_CURRENT_BINARY_DIR}" OPTIONS "--no-root" "--only" "main")
 elseif("${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml" IS_NEWER_THAN "${CMAKE_CURRENT_BINARY_DIR}/poetry.lock")
     file(COPY "${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml" DESTINATION "${CMAKE_CURRENT_BINARY_DIR}")
     poetry_update(DIR "${CMAKE_CURRENT_BINARY_DIR}" OPTIONS "--no-dev")
