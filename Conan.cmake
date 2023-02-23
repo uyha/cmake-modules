276ee56701755283bd4c488bc3e833a4b1014e8e
Uy Ha <hchanuy@gmail.com>
2023-02-23 15:03:58 +0000

    remove errorenous quotes and update Conan.cmake

--- Conan.cmake
+++ Conan.cmake
@@ -4,25 +4,18 @@ if(NOT USE_CONAN)
 endif()
 
 find_package(Conan REQUIRED)
-download_conan_cmake(conan_cmake DIR ${CMAKE_BINARY_DIR})
-include(${conan_cmake})
-if(${CMAKE_CURRENT_SOURCE_DIR}/conanfile.txt IS_NEWER_THAN ${CMAKE_CURRENT_BINARY_DIR}/conan.lock)
-  file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/conanfile.txt DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-  if(DEFINED TARGET_ARCH)
-    conan_cmake_autodetect(settings ARCH "${TARGET_ARCH}")
-  else()
-    conan_cmake_autodetect(settings)
-  endif()
-  conan_cmake_run(
-    CONANFILE conanfile.txt
-    BUILD missing
-    SETTINGS ${settings}
-  )
-endif()
+conan_install(
+  "${CMAKE_CURRENT_SOURCE_DIR}"
+  OUTPUT_FOLDER "${CMAKE_CURRENT_BINARY_DIR}"
+  GENERATOR CMakeDeps
+  SETTING_BUILD build_type=Release
+  SETTING_HOST build_type=${CMAKE_BUILD_TYPE}
+  BUILD missing
+  VERBOSE quiet
+)
+list(APPEND CMAKE_PREFIX_PATH "${CMAKE_CURRENT_BINARY_DIR}")
 set_property(
   DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
   APPEND
   PROPERTY CMAKE_CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/conanfile.txt
 )
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_BINARY_DIR})
-set(CONAN_CMAKE_SILENT_OUTPUT TRUE)
