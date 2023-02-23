6d88e26ba9578ef61293000e9f065ad2b5dd0f44
Uy Ha <hchanuy@gmail.com>
2023-02-23 12:57:16 +0000

    add `conan_install` function to wrap `conan install` call

--- FindConan.cmake
+++ FindConan.cmake
@@ -10,13 +10,13 @@ if(Conan_EXECUTABLE)
       COMMAND cmd /C ${Conan_EXECUTABLE} --version
       RESULT_VARIABLE result
       OUTPUT_VARIABLE version
-    )
+      )
   else()
     execute_process(
       COMMAND ${Conan_EXECUTABLE} --version
       RESULT_VARIABLE result
       OUTPUT_VARIABLE version
-    )
+      )
   endif()
   if(result EQUAL 0)
     if(${version} MATCHES ".*version(.*)")
@@ -24,27 +24,71 @@ if(Conan_EXECUTABLE)
     endif()
   endif()
 
-  function(download_conan_cmake out)
-    set(options ";")
-    set(single_values DIR)
-    set(multi_values ";")
+  macro(_conan_add_flag option flag)
+    if(arg_${option})
+      list(APPEND optional_arguments ${flag})
+    endif()
+  endmacro()
+
+  macro(_conan_add_value option flag)
+    if(arg_${option})
+      list(APPEND optional_arguments ${flag} "\"${arg_${option}}\"")
+    endif()
+  endmacro()
+
+  macro(_conan_add_values option flag)
+    if(arg_${option})
+      foreach(var IN LISTS arg_${option})
+        list(APPEND optional_arguments ${flag} "\"${var}\"")
+      endforeach()
+    endif()
+  endmacro()
+
+  function(conan_install)
+    set(options LOGGER UPDATE NO_REMOTE LOCKFILE_PARTIAL LOCKFILE_PACKAGES LOCKFILE_CLEAN DEPLOY)
+    set(single_values FORMAT VERBOSE NAME VERSION USER CHANNEL BUILD PROFILE_BUILD PROFILE_HOST LOCK_FILE LOCKFILE_OUT OUTPUT_FOLDER)
+    set(multi_values REQUIRES TOOL_REQUIRES REMOTE OPTION_BUILD OPTION_HOST SETTING_BUILD SETTING_HOST CONF_BUILD CONF_HOST GENERATOR)
     cmake_parse_arguments(arg "${options}" "${single_values}" "${multi_values}" ${ARGN})
-    if(NOT DEFINED arg_DIR)
-      set(arg_DIR "${CMAKE_CURRENT_BINARY_DIR}")
-      message(NOTICE "DIR is not set, defaulting to ${arg_DIR}")
+
+    if(arg_NO_REMOTE AND arg_REMOTE)
+      message(FATAL_ERROR "NO_REMOTE and REMOTE cannot be specified at the same time")
     endif()
 
-    if(NOT EXISTS ${arg_DIR}/conan.cmake)
-      file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/0.18.1/conan.cmake"
-        "${arg_DIR}/conan.cmake"
-        STATUS download_status)
-      list(POP_FRONT download_status download_error)
-      if(NOT download_error EQUAL 0)
-        file(REMOVE ${arg_DIR}/conan.cmake)
-        message(FATAL_ERROR "Failed to download conan.cmake")
-      endif()
-    endif()
-    set(${out} ${arg_DIR}/conan.cmake PARENT_SCOPE)
+    _conan_add_flag(LOGGER --logger)
+    _conan_add_flag(UPDATE --update)
+    _conan_add_flag(NO_REMOTE --no-remote)
+    _conan_add_flag(LOCKFILE_PARTIAL --lockfile-partial)
+    _conan_add_flag(LOCKFILE_PACKAGES --lockfile-packages)
+    _conan_add_flag(LOCKFILE_CLEAN --lockfile-clean)
+    _conan_add_flag(DEPLOY --deploy)
+
+    _conan_add_value(FORMAT --format)
+    _conan_add_value(VERBOSE -v)
+    _conan_add_value(NAME --name)
+    _conan_add_value(VERSION --version)
+    _conan_add_value(USER --user)
+    _conan_add_value(CHANNEL --channel)
+    _conan_add_value(BUILD --build)
+    _conan_add_value(PROFILE_BUILD --profile:build)
+    _conan_add_value(PROFILE_HOST --profile:host)
+    _conan_add_value(LOCK_FILE --lockfile)
+    _conan_add_value(LOCKFILE_OUT --lockfile-out)
+    _conan_add_value(OUTPUT_FOLDER --output-folder)
+
+    _conan_add_values(REQUIRES --requires)
+    _conan_add_values(TOOL_REQUIRES --tool-requires)
+    _conan_add_values(REMOTE --remote)
+    _conan_add_values(OPTION_BUILD --options:build)
+    _conan_add_values(OPTION_HOST --options:host)
+    _conan_add_values(SETTING_BUILD --settings:build)
+    _conan_add_values(SETTING_HOST --settings:host)
+    _conan_add_values(CONF_BUILD --conf:build)
+    _conan_add_values(CONF_HOST --conf:host)
+    _conan_add_values(GENERATOR --generator)
+
+    execute_process(
+      COMMAND "${Conan_EXECUTABLE}" install ${optional_arguments} ${arg_UNPARSED_ARGUMENTS}
+      )
   endfunction()
 endif()
 
