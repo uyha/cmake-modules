0a2a6354f6f357344511476f9d4db1af3c5114de
Uy Ha <hchanuy@gmail.com>
2023-03-11 17:41:55 +0100

    add poetry_lock command and simplify poetry usage logic

--- Poetry.cmake
+++ Poetry.cmake
@@ -1,25 +1,25 @@
 find_package(Poetry REQUIRED)
 
-if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml")
+set(poetry_conf "${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml")
+
+if(NOT EXISTS "${poetry_conf}")
   message(STATUS "No pyproject.toml found, skipping setting Poetry up")
   return()
 endif()
 
-poetry_path(Python_ROOT_DIR DIR "${CMAKE_CURRENT_SOURCE_DIR}")
-
-if(Python_ROOT_DIR STREQUAL "" OR NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/poetry.lock")
+if("${poetry_conf}" IS_NEWER_THAN "${CMAKE_CURRENT_SOURCE_DIR}/poetry.lock")
+  poetry_lock(DIR "${CMAKE_CURRENT_SOURCE_DIR}" NO_UPDATE)
   poetry_install(DIR "${CMAKE_CURRENT_SOURCE_DIR}" OPTIONS "--no-root")
-  poetry_path(Python_ROOT_DIR DIR "${CMAKE_CURRENT_SOURCE_DIR}")
-# IS_NEWER_THAN returns true when both files have the same timestamp, the inverted condition is needed here
-elseif(NOT "${CMAKE_CURRENT_SOURCE_DIR}/poetry.lock" IS_NEWER_THAN "${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml")
-  poetry_update(DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+  execute_process(COMMAND "${CMAKE_COMMAND}" -E touch "${CMAKE_CURRENT_SOURCE_DIR}/poetry.lock")
 endif()
+
+poetry_path(Python_ROOT_DIR DIR "${CMAKE_CURRENT_SOURCE_DIR}")
 
 set(Python_ROOT_DIR "${Python_ROOT_DIR}" CACHE PATH "Path to the root directory of the target Python interpreter")
 
 set_property(
   DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
   APPEND
-  PROPERTY CMAKE_CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/pyproject.toml"
+  PROPERTY CMAKE_CONFIGURE_DEPENDS "${poetry_conf}"
   )
 
